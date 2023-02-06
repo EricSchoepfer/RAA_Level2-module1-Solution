c76173b2c35e2f69b7fc647c1964ba18632d40ca
Eric Schoepfer <eric.schoepfer@gmail.com>
2023-02-06 12:58:46 -0800

    Add project files.

---
+++ RAA_Level2-module1-Solution/Forms/MyForm.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RAA_Level2_module1_Solution.Forms
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public String Directory;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Directory = "";
+
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+// Do something interesting
+
+List<string> deletedfilelog = new List<string>();
+deletedfilelog.Add("The Following backup files have been deleted:");
